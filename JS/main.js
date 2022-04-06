206ae50ac2c66ff8474470db42ae0f81f544b1e4
Alex04412 <77611774+Alex04412@users.noreply.github.com>
2022-04-06 15:50:27 +0200

    Dark Switch

--- JS/main.js
+++ JS/main.js
@@ -6,7 +6,12 @@ var dark = false;
 var top1 = $("#top");
 var artiklar = $("#artiklar");
 var info1 = $("#info");
-var head1 = $("header")
+var sök = $("#sök");
+var nyheter = $("#nyheter");
+var head1 = $("header");
+var topimg =$("#topimg");
+var lightrad = $("#Light");
+var darkrad = $("#Dark");
 
 
 
@@ -17,6 +22,14 @@ switch1.click(function(){
       head1.css("background-color", "#3d3d3d");
       artiklar.css("background-color", "#3d3d3d");
       info1.css("background-color", "#3d3d3d");
+      sök.css("background-color", "#3d3d3d");
+      switch1.css("background-color", "#3d3d3d");
+      nyheter.css("background-color", "#3d3d3d");
+      top1.css("background-color", "#3d3d3d");
+      topimg.css("background-color", "#3d3d3d");
+
+
+      head1.css("color", "#ffffff");
   }
   else {
     dark=false;
@@ -24,6 +37,13 @@ switch1.click(function(){
     head1.css("background-color", "green");
     artiklar.css("background-color", "green");
     info1.css("background-color", "green");
+    sök.css("background-color", "green");
+    switch1.css("background-color", "white");
+    nyheter.css("background-color", "green");
+    top1.css("background-color", "white");
+    topimg.css("background-color", "white");
+
+    head1.css("color", "black");
   }
 
 });
