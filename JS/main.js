2ad1bfa163531929ee29e1d5bf9a0fd7d0917afc
Alex04412 <77611774+Alex04412@users.noreply.github.com>
2022-04-06 15:12:12 +0200

    a

--- JS/main.js
+++ JS/main.js
@@ -6,7 +6,7 @@ var dark = false;
 var top1 = $("#top");
 var artiklar = $("#artiklar");
 var info1 = $("#info");
-var class1 = $("header")
+var head1 = $("header")
 
 
 
@@ -14,14 +14,16 @@ switch1.click(function(){
   if (dark==false) {
     dark=true;
       console.log("dark on");
-      class1.css("background-color", "#3d3d3d");
+      head1.css("background-color", "#3d3d3d");
       artiklar.css("background-color", "#3d3d3d");
       info1.css("background-color", "#3d3d3d");
   }
   else {
     dark=false;
     console.log("dark off");
-    class1.css("background-color", "white");
+    head1.css("background-color", "green");
+    artiklar.css("background-color", "green");
+    info1.css("background-color", "green");
   }
 
 });
